b15087c1f3f6484d590bd47cb9aa6b730da2cc10
albanyprojects <jayden.wren@albanyproducts.co.uk>
2026-08-19 09:17:25 +0100

    Create main.rs
    just allows the code to compile with no errors for now

---
+++ main.rs
@@ -0,0 +1,15 @@
+#![allow(dead_code)]
+#![allow(unused_imports)]
+#![allow(unused_variables)]
+
+mod calendar;
+mod employee;
+mod managing;
+mod booking;
+
+fn main() {
+    println!("hello world")
+}
+
+
+
